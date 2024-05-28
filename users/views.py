b39dbc6d20dde619f1bbfea84ece80fdf63254db
nokekip <kipnoke@gmail.com>
2024-05-29 00:52:11 +0300

    Validate  and login user

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
 from .forms import RegisterUserForm
 
 
@@ -12,7 +12,7 @@ def register(request):
         if form.is_valid:
             user = form.save()
             messages.success(request, 'Acount has been created succesfully')
-            return redirect('login')
+            return redirect('login_user')
         else:
             for field, errors in form.errors.items():
                 for error in errors:
@@ -26,3 +26,18 @@ def register(request):
         return render(request, 'users/register_user.html', context)
 
 
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None and user.is_active:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('index')
+        else:
+            messages.warning(request, 'Invalid credentials')
+            return redirect('login_user')
+
+    return render(request, 'users/login_user.html')
