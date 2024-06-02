d98d2f9eaa3e83ff29f46113612f1fd8cba28c4a
nokekip <kipnoke@gmail.com>
2024-06-02 13:02:39 +0300

    created model for forum to facilitate chat features

--- community/models.py
+++ community/models.py
@@ -7,4 +7,20 @@ class Region(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.nam
+        return self.name
+
+class Forum(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(blank=True, null=True)
+    members = models.ManyToManyField('users.User', related_name='forums')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+
+    def __str__(self):
+        return self.name
