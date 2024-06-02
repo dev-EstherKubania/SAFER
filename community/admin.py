17c751c2d6f474c8d0dffe242f1b9a043dfcfbcc
nokekip <kipnoke@gmail.com>
2024-06-02 13:40:55 +0300

    register community models to admin site

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Region, Forum, Message, Media
+
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Forum)
+admin.site.register(Media)
+admin.site.register(Message)
