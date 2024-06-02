9725aab14bc6672617be5bb7fb3fab4ca2e52d57
nokekip <kipnoke@gmail.com>
2024-06-02 13:54:22 +0300

    added url for media upload to receive data from javascript

--- community/urls.py
+++ community/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.community_home, name = 'community-home'),
     path('forum/<str:forum_id>',views.forum, name = 'forum'),
+    path('upload_media/',views.upload_media, name = 'upload_media'),
 ]
