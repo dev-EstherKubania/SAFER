d7bc7837e71426a444ebd4664aaf29928037f244
nokekip <kipnoke@gmail.com>
2024-06-03 21:00:02 +0300

    update auth url to allow django redirection

--- safer/urls.py
+++ safer/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather_forecast.urls')),
-    path('users/', include('users.urls')),
+    path('accounts/', include('users.urls')),
     path('community/', include('community.urls')),
 ]
 
